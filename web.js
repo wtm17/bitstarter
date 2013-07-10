5b1aaffdaaf699116344a9a5204bb89a53767a11
Billy Meyers <meyers.billy@gmail.com>
2013-07-10 04:44:00 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var message = fs.readFileSync(web.js).toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
