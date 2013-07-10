440e28a6a7846fbf4627b4c9ffc9b14b8ecfa50c
Billy Meyers <meyers.billy@gmail.com>
2013-07-10 04:47:20 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
 var message = fs.readFileSync(web.js).toString();
 
 app.get('/', function(request, response) {
